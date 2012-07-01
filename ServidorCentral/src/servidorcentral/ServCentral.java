d6bd64c812ad6922deef2ba86d9b2adf4cb5273a
Tefa Jung <tefa@Tefa.(none)>
2012-06-30 23:02:10 -0430

    mail arreglado

---
+++ ServidorCentral/src/servidorcentral/ServCentral.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidorcentral;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import javax.mail.MessagingException;
+/**
+ *
+ * @author Omar El Assaad
+ */
+public class ServCentral {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException, MessagingException {
+        ServerSocket serverSocket = null;
+        try{
+            serverSocket = new ServerSocket(4445);
+        } catch(IOException e){
+            System.err.println("No se pudo escuchar del puerto: 4445.");
+            System. exit(1);
+        }
+
+        Socket clientSocket = null;
+
+        while(true) {
+            clientSocket = serverSocket.accept();
+            Manejador manejador = new Manejador(clientSocket);
+            manejador.start();
+
+        }
+
+    }
+}
